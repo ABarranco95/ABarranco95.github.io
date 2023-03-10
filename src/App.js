28177b0a5f7ac43200a44b18f4d1e6c3c08bccb1
Angel <abarranco650@gmail.com>
2023-03-10 13:52:04 -0800

    app.js refactor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/work" component={Work} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }

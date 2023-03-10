67a691fa0066de5b02c9d7d0c23537616720a8ba
Angel <abarranco650@gmail.com>
2023-03-10 10:07:21 -0800

    update build

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,7 +22,7 @@ const Work = () => {
       description:
         "Team Link is an app designed for gamers and e-sports enthusiasts to connect and form teams. Similar to Tinder, users can swipe through potential matches and connect with others who are interested in playing the same games. With features like messaging and team creation, Team Link makes it easy for gamers to find and connect with like-minded players. This React Native app is built using Tailwind CSS, Firebase, and React Icons.",
       technologies: ["React Native", "Tailwind CSS", "Firebase", "React Icons"],
-      demoLink: "https://example.com",
+      demoLink: "/",
       codeLink: "https://github.com/anthonyperry3/team-link-app",
     },
     {
@@ -31,7 +31,7 @@ const Work = () => {
       description:
         "Task Game is a unique todo app that adds a fun twist to completing tasks. With this app, you earn points for each task you complete, which can be used to climb the leaderboard and compete with friends. Featuring a sleek user interface and built with React Native, Tailwind CSS, and Firebase, Task Game is the perfect way to gamify your productivity. ",
       technologies: ["React Native", "Tailwind CSS", "Firebase", "React Icons"],
-      demoLink: "https://example.com",
+      demoLink: "/",
       codeLink: "https://github.com/ABarranco95/game-todo-app",
     },
     {
@@ -40,7 +40,7 @@ const Work = () => {
       description:
         "A classic game of ping pong built with vanilla javascript! Can you beat our AI champion?",
       technologies: ["Javascript", "DOM", "HTML", "CSS"],
-      demoLink: "https://example.com",
+      demoLink: "/",
       codeLink: "https://github.com/ABarranco95/Ping-Pong",
     },
   ];
